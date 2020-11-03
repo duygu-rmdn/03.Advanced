4e431d1650fc29f3f5ef008f7261f5c991dbe68c
unknown <duygumetin99@gmail.com>
2020-11-03 23:12:01 +0200

    ex11

--- EXERCISE- FUNCTIONAL PROGRAMMING/08. Custom Comparator/Program.cs
+++ EXERCISE- FUNCTIONAL PROGRAMMING/08. Custom Comparator/Program.cs
@@ -11,23 +11,25 @@ namespace _08._Custom_Comparator
                 .Split()
                 .Select(int.Parse)
                 .ToArray();
-            Array.Sort(nums, (x, y) =>
-            {
-                int sorted = 0;
-                if (x % 2 == 0 && y % 2 !=0)
-                {
-                    sorted = 1;
-                }
-                else if (x % 2 != 0 && y % 2 == 0)
-                {
-                    sorted = -1;
-                }
-                else
-                {
-                    sorted = x - y;
-                }
-                return sorted;
-            });
+            //Array.Sort(nums, (x, y) =>
+            //{
+            //    int sorted = 0;
+            //    if (x % 2 == 0 && y % 2 !=0)
+            //    {
+            //        sorted = -1;
+            //    }
+            //    else if (x % 2 != 0 && y % 2 == 0)
+            //    {
+            //        sorted = 1;
+            //    }
+            //    else
+            //    {
+            //        sorted = x - y;
+            //    }
+            //    return sorted;
+            //});
+            Array.Sort(nums, (x, y) => x % 2 == 0 && y % 2 != 0 ? -1 : x % 2 != 0 && y % 2 == 0 ? 1 : x.CompareTo(y));
+
             Console.WriteLine(string.Join(" ", nums));
         }
     }
---
+++ EXERCISE- FUNCTIONAL PROGRAMMING/11. The Party Reservation Filter Module/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11._The_Party_Reservation_Filter_Module
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> peoples = Console.ReadLine()
+                .Split()
+                .ToList();
+
+            string command = Console.ReadLine();
+            List<string> filters = new List<string>();
+            while (command != "Print")
+            {
+                string[] spleted = command.Split(";");
+                string operation = spleted[0];
+                string filter = spleted[1];
+                string argument = spleted[2];
+
+                if (operation == "Add filter")
+                {
+                    filters.Add($"{filter};{argument}");
+                }
+                else if(operation == "Remove filter")
+                {
+                    filters.Remove($"{filter};{argument}");
+                }
+
+                command = Console.ReadLine();
+            }
+            foreach (var item in filters)
+            {
+                string[] splited = item.Split(";");
+                string filterType = splited[0];
+                string argument = splited[1];
+
+                if (filterType == "Starts with")
+                {
+                    peoples = peoples.Where(p => !p.StartsWith(argument)).ToList();
+                }
+                else if (filterType == "Ends with")
+                {
+                    peoples = peoples.Where(p => !p.EndsWith(argument)).ToList();
+                }
+                else if (filterType == "Length")
+                {
+                    peoples = peoples.Where(p => p.Length != int.Parse(argument)).ToList();
+                }
+                else if(filterType == "Contains")
+                {
+                    peoples = peoples.Where(p => !p.Contains(argument)).ToList();
+                }
+
+            }
+            Console.WriteLine(string.Join(" ", peoples));
+
+        }
+    }
+}
