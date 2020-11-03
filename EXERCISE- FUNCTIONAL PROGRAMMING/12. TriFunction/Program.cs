5c376c79ed8c9206f9dd66cf88c1b6f7d225195f
unknown <duygumetin99@gmail.com>
2020-11-03 23:36:25 +0200

    ex12

---
+++ EXERCISE- FUNCTIONAL PROGRAMMING/12. TriFunction/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12._TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> peoples = Console.ReadLine()
+                .Split()
+                .ToList();
+
+            Func<string, int> getSum = p => p.Select(c => (int)c).Sum();
+            Func<List<string>, int, Func<string, int>, string> predicate = (peoples, n, func) => peoples.FirstOrDefault(p => func(p) >= n);
+            string result = predicate(peoples, n, getSum);
+            Console.WriteLine(result);
+        }
+        
+    }
+}
