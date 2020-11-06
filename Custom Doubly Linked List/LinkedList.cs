9ed02a83c84334c0817f4b91729b84f70a9ad4e1
unknown <duygumetin99@gmail.com>
2020-11-06 23:59:57 +0200

    d

---
+++ Custom Doubly Linked List/LinkedList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Custom_Doubly_Linked_List
+{
+    public class LinkedList
+    {
+        public Node Head { get; set; }
+
+        public void AddHead(Node node)
+        {
+            node.Next = Head;
+            Head = node;
+        }
+        public void PrintList()
+        {
+            Node currentNode = Head;
+            while (currentNode != null)
+            {
+                Console.WriteLine(currentNode.Value);
+                currentNode = currentNode.Next;
+            }
+        }
+    }
+}
---
+++ Custom Doubly Linked List/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Custom_Doubly_Linked_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+
+            for (int i = 0; i < 20; i++)
+            {
+                list.AddHead(new Node(i));
+            }
+            list.PrintList();
+        }
+    }
+}
