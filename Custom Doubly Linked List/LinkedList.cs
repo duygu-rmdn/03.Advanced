3bd0234bd5425fe0b63cbc78a7541005c9928eda
unknown <duygumetin99@gmail.com>
2020-11-11 21:37:42 +0200

    LinkedList is finished

--- Custom Doubly Linked List/LinkedList.cs
+++ Custom Doubly Linked List/LinkedList.cs
@@ -10,27 +10,85 @@ namespace Custom_Doubly_Linked_List
 
         public Node Tail { get; set; }
 
-        public void AddHead(Node node)
+        public void ForEach(Action<Node> action)
         {
-            if (Head == null)
+            Node curNode = Head;
+            while (curNode != null)
             {
-                Head = node;
-                Tail = node;
-            }
-            else
-            {
-                node.Next = Head;
-                Head.Previous = node;
-                Head = node;
+                action(curNode);
+                curNode = curNode.Next;
             }
         }
 
-        public Node Pop()
+        public Node[] ToArray()
+        {
+            List<Node> list = new List<Node>();
+            this.ForEach(node => list.Add(node));
+            return list.ToArray();
+        }
+
+        public bool Remove(int value)
+        {
+            Node curNode = Head;
+            while (curNode != null)
+            {
+                if (curNode.Value == value)
+                {
+                    curNode.Previous.Next = curNode.Next;
+                    curNode.Next.Previous = curNode.Previous;
+                    return true;
+                }
+                curNode = curNode.Next;
+            }
+            return false;
+        }
+
+        public void AddHead(Node newHead)
+        {
+            if (Head == null)
+            {
+                Head = newHead;
+                Tail = newHead;
+            }
+            else
+            {
+                newHead.Next = Head;
+                Head.Previous = newHead;
+                Head = newHead;
+            }
+        }
+
+        public void AddLast(Node newTail)
+        {
+            if (Tail == null)
+            {
+                Tail = newTail;
+                Head = newTail;
+            }
+            else
+            {
+                newTail.Previous = Tail;
+                Tail.Next = newTail;
+                Tail = newTail;
+            }
+        }
+
+        public Node RemoveFirst()
         {
             var oldHead = this.Head;
             this.Head = this.Head.Next;
+            Head.Previous = null;
             return oldHead;
         }
+
+        public Node RemoveLast()
+        {
+            var oldTail = this.Tail;
+            Tail = this.Tail.Previous;
+            Tail.Next = null;
+            return oldTail;
+        }
+
         public int Peek()
         {
             return this.Head.Value;
@@ -38,13 +96,15 @@ namespace Custom_Doubly_Linked_List
 
         public void PrintList()
         {
-            Node currentNode = Head;
-            while (currentNode != null)
-            {
-                Console.WriteLine(currentNode.Value);
-                currentNode = currentNode.Next;
-            }
+            this.ForEach(node => Console.WriteLine(node.Value));
+            //Node currentNode = Head;
+            //while (currentNode != null)
+            //{
+            //    Console.WriteLine(currentNode.Value);
+            //    currentNode = currentNode.Next;
+            //}
         }
+
         public void ReversePrintList()
         {
             Node currNode = Tail;
--- Custom Doubly Linked List/Program.cs
+++ Custom Doubly Linked List/Program.cs
@@ -6,18 +6,51 @@ namespace Custom_Doubly_Linked_List
     {
         static void Main(string[] args)
         {
-            LinkedList list = new LinkedList();
+           
 
-            for (int i = 0; i < 10; i++)
+            LinkedList list = new LinkedList();
+            for (int i = 0; i < 5; i++)
             {
                 list.AddHead(new Node(i));
             }
+            list.Remove(2);
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    list.AddLast(new Node(i));
+            //}
+
+            //list.RemoveFirst();
+            //list.RemoveFirst();
+            //list.RemoveFirst();
+
+            //list.RemoveLast();
+            //list.RemoveLast();
+            //list.RemoveLast();
+
             list.PrintList();
-            //Console.WriteLine($"Poped: {list.Pop().Value}");
-            //Console.WriteLine($"Poped: {list.Pop().Value}");
-            //Console.WriteLine($"Poped: {list.Pop().Value}");
-            //Console.WriteLine($"Poped: {list.Pop().Value}");
-            list.ReversePrintList();
+
+            Console.WriteLine(list.ToArray().Length);
+            
+
+
+
+
+
+
+
+
+            //LinkedList list = new LinkedList();
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    list.AddHead(new Node(i));
+            //}
+            //list.PrintList();
+            ////Console.WriteLine($"Poped: {list.Pop().Value}");
+            ////Console.WriteLine($"Poped: {list.Pop().Value}");
+            ////Console.WriteLine($"Poped: {list.Pop().Value}");
+            ////Console.WriteLine($"Poped: {list.Pop().Value}");
+            //list.ReversePrintList();
         }
     }
 }
