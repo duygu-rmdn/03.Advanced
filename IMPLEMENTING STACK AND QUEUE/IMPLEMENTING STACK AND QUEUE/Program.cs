713ecb213d541ad2bfc31558927b6c2c08a9126b
unknown <duygumetin99@gmail.com>
2020-11-15 23:48:01 +0200

    Created new demo

---
+++ IMPLEMENTING STACK AND QUEUE/IMPLEMENTING STACK AND QUEUE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IMPLEMENTING_STACK_AND_QUEUE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
