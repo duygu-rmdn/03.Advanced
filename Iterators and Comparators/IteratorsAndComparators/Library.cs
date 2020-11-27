2763db0633bcf7356c607a7bbc9a9e7e46161e81
unknown <duygumetin99@gmail.com>
2020-11-27 23:17:53 +0200

    Iteretours and Comparatours - Part 1

---
+++ Iterators and Comparators/IteratorsAndComparators/Library.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    class Library
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+        
+    }
+}
